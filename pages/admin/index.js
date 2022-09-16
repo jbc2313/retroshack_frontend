488464d3180716de2d967f64602471698aa17e53
jbc2313 <jamesbcorbin@gmail.com>
2022-09-15 22:39:01 -0500

    added admin page

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import axios from 'axios'
+import { unstable_getServerSession } from "next-auth/next"
+import { authOptions } from '../api/auth/[...nextauth]'
+import { Button } from 'primereact/button'
+
+const Index = ({ isAdmin, userInfo }) => {
 
 
-const Index = () => {
-
-
-
+  console.log(isAdmin)
+  console.log(userInfo, 'userinfo')
 
   return (
-    <div>
-
-      Admin Page
-      This will be guared by auth
-
+    <div style={{height: '100vh'}}>
+      {isAdmin && <>
+      <h1 style={{textAlign: 'center'}}>WELCOME ADMIN</h1>
+      <div style={{ display: 'flex', justifyContent: 'center'}}>
+        <Button>ADD ITEM</Button>
+        <Button>UPDATE ITEM</Button>
+      </div>
+      
+      
+      
+      
+      
+      </>}
     </div>
 
 
@@ -21,3 +32,42 @@ const Index = () => {
 
 
 export default Index
+
+
+export async function getServerSideProps(context) {
+
+  let isAdmin = false
+  const session = await unstable_getServerSession(context.req, context.res, authOptions)
+  if (session) {
+    isAdmin = true
+    const userData = {
+      email: session.user.email
+    }
+    const { data } = await axios.post('http://localhost:7777/find/user', userData)
+    const userInfo = await data
+    if(userInfo.isAdmin === false) {
+      return {
+        props: {
+          isAdmin: isAdmin
+        }
+      }
+    } else {  
+      return {
+        props: {
+          isAdmin: isAdmin,
+          userInfo: userInfo
+        }
+      }
+    }
+
+
+  } else {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
+}
