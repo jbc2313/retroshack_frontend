c1a8a62f48760c053b7dab6daca93dfdface4056
jbc2313 <jamesbcorbin@gmail.com>
2022-09-16 19:36:17 -0500

    added about page

---
+++ pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>We love retro computers, And we hope you do to.</h1>
+
+
+
+
+    </div>
+  )
+}
+
+export default About
